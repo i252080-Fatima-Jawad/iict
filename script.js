1275b5ef8ae1e5bf7f8156347094dc13b5a936b5
i252080-Fatima-Jawad <i252080@isb.nu.edu.pk>
2025-11-30 16:02:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function login() 
+{
+    loginForm.style.display = "block";
+    signupForm.style.display = "none";
+}
+
+function signup() 
+{
+    loginForm.style.display = "none";
+    signupForm.style.display = "block";
+}
+
+ function custom() 
+ { 
+ console.log("Login"); 
+ window.alert("Login Successful ! ");
+} 
+ function custom2() 
+ { 
+ console.log("Sign up"); 
+ window.alert("Sign up Successful ! ");  } 
