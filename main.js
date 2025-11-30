6b2969284fce18539e884ba2ce1b8feb112d4827
i256541 <i256541@isb.nu.edu.pk>
2025-11-30 16:10:48 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("Website loaded successfully!");
+
+function addToCart() {
+    alert("Item added to your cart successfully!");
+    console.log("User added an item to the cart.");
+}
+
+function openCategory(categoryName) {
+    alert("You are viewing the " + categoryName + " collection.");
+    console.log("User clicked on category: " + categoryName);
+}
+
+function checkCart() {
+    alert("Your cart is currently empty. Start shopping!");
+    console.log("User checked the cart.");
+}
